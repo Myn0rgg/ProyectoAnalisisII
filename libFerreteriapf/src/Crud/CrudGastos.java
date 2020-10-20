45a9fe626eeddd552896a64b8f1b03ad9d901297
Diego Raymundo <diegor_137@outlook.es>
2020-10-20 11:53:58 -0600

    Se creo el insert del crudGastos

--- libFerreteriapf/src/Crud/CrudGastos.java
+++ libFerreteriapf/src/Crud/CrudGastos.java
@@ -5,10 +5,42 @@
  */
 package Crud;
 
+import Pojos.TbGastos;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author mynor
  */
 public class CrudGastos {
+    
+    public static boolean insert(String nombre){
+        boolean bandera=false;
+        Session session=HibernateUtil.HibernateUtil.getSessionFactory().openSession();
+        Criteria criteria=session.createCriteria(TbGastos.class);
+        TbGastos insert=(TbGastos)criteria.uniqueResult();
+        Transaction transaccion=null;
+        try {
+            transaccion=session.beginTransaction();
+            if(insert==null){
+                insert=new TbGastos();
+                insert.setEstado(true);
+                insert.setNomGasto(nombre);
+                session.save(insert);
+                bandera=true;
+            }
+            transaccion.commit();
+        } catch (HibernateException e) {
+            transaccion.rollback();
+            System.out.println("error"+e);
+        }finally{
+            session.close();
+        }
+        return bandera;
+        
+    }
     
 }
